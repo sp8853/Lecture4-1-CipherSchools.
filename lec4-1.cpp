7d916b9eea74039c58b23e0447115515dbd68416
sp8853 <103991911+sp8853@users.noreply.github.com>
2023-08-08 23:25:24 +0530

    Add files via upload

---
+++ lec4-1.cpp
@@ -0,0 +1,23 @@
+Conditional statements- a statement which is either true/false
+
+got answers for the questions 
+
+1. why do we need conditional statement in c++
+2. how can we implement conditional statements
+3.introduction to boolean algebra
+4. if-else types
+5. switch case
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"input integers a and b"
+cin>>a;
+cin>>b;
+// statement :a>b
+cout<<a>b;
+
+}
